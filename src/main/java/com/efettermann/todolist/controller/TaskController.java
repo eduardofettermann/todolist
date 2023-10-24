6d0943765a419d536e0e816145174f017bc5ee39
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-10-24 19:05:39 -0300

    feat: adiciona atributo TaskService na classe TaskController

--- src/main/java/com/efettermann/todolist/controller/TaskController.java
+++ src/main/java/com/efettermann/todolist/controller/TaskController.java
@@ -1,10 +1,14 @@
 package com.efettermann.todolist.controller;
 
+import com.efettermann.todolist.service.TaskService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
+@AllArgsConstructor
 public class TaskController {
+    private TaskService taskService;
 
 }
