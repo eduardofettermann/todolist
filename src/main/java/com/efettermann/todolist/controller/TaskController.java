a89b3497a15d9781687af7ac0a6db5bf9a7c3407
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-10-24 19:23:08 -0300

    feat(Controller/Service): adiciona metodo getTasks(GET)

--- src/main/java/com/efettermann/todolist/controller/TaskController.java
+++ src/main/java/com/efettermann/todolist/controller/TaskController.java
@@ -3,10 +3,9 @@ package com.efettermann.todolist.controller;
 import com.efettermann.todolist.model.Task;
 import com.efettermann.todolist.service.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,7 +14,12 @@ public class TaskController {
     private TaskService taskService;
 
     @PostMapping
-    public Task saveTask(@RequestBody Task task){
+    public Task saveTask(@RequestBody Task task) {
         return taskService.saveTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return taskService.getTasks();
     }
 }
--- src/main/java/com/efettermann/todolist/service/TaskService.java
+++ src/main/java/com/efettermann/todolist/service/TaskService.java
@@ -5,6 +5,8 @@ import com.efettermann.todolist.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class TaskService {
@@ -12,5 +14,9 @@ public class TaskService {
 
     public Task saveTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
     }
 }
