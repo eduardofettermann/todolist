41d65447d3b1e10d9390757a09af9ebbed6f6601
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-10-24 19:12:53 -0300

    feat: declara TaskRepository como repositorio(banco de dados)

--- src/main/java/com/efettermann/todolist/service/TaskService.java
+++ src/main/java/com/efettermann/todolist/service/TaskService.java
@@ -1,7 +1,12 @@
 package com.efettermann.todolist.service;
 
+import com.efettermann.todolist.model.Task;
+import com.efettermann.todolist.repository.TaskRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TaskService {
+    private TaskRepository taskRepository;
 }
