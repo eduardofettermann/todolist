5dcb854e1e139fbf1828306cd0ef0ead3dcc8f5c
Eduardo Fettermann <eduardofettermann212@gmail.com>
2023-10-24 19:53:00 -0300

    feat(Controller/Service): adiciona metodo changeTask(PUT)

--- src/main/java/com/efettermann/todolist/controller/TaskController.java
+++ src/main/java/com/efettermann/todolist/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.efettermann.todolist.controller;
 
 import com.efettermann.todolist.model.Task;
+import com.efettermann.todolist.model.UpdateTask;
 import com.efettermann.todolist.service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +22,11 @@ public class TaskController {
     @GetMapping
     public List<Task> getTasks() {
         return taskService.getTasks();
+    }
+
+    @PutMapping("/{id}")
+    public Task changeTask(@RequestBody UpdateTask updateTask, @PathVariable("id") String id) throws Exception {
+        return taskService.updateTask(updateTask,id);
+
     }
 }
--- src/main/java/com/efettermann/todolist/service/TaskService.java
+++ src/main/java/com/efettermann/todolist/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.efettermann.todolist.service;
 
 import com.efettermann.todolist.model.Task;
+import com.efettermann.todolist.model.UpdateTask;
 import com.efettermann.todolist.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,13 @@ public class TaskService {
 
     public List<Task> getTasks() {
         return taskRepository.findAll();
+    }
+
+    public Task updateTask(UpdateTask updateTask, String id) throws Exception{
+        Task taskToUpdate = taskRepository.findById(id)
+                .orElseThrow(() -> new Exception("O id não foi encontrado no banco de dados"));
+        taskToUpdate.setTitle(updateTask.getTitle());
+        taskToUpdate.setDescription(updateTask.getDescription());
+        return taskRepository.save(taskToUpdate);
     }
 }
